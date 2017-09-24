44645bafb8b3bccacf81ea5ef760d115827c7290
Alan Bondarchuk <imacoda@gmail.com>
2017-09-25 00:27:46 +0500

    Add type hint

--- src/Exception/ExchangeException.php
+++ src/Exception/ExchangeException.php
@@ -3,12 +3,13 @@
 namespace Money;
 
 
+use Currency\Currency;
 use Money\Exception\MoneyException;
 
 class ExchangeException extends MoneyException
 {
 
-    public static function createFromCurrencies($baseCurrency, $counterCurrency)
+    public static function createFromCurrencies(Currency $baseCurrency, Currency $counterCurrency)
     {
         $message = sprintf(
             'Cannot exchange a currency pair: %s/%s',
