20c4b28b4c0685d8787ceeeb5bdc296f881a7e2e
Alan Bondarchuk <imacoda@gmail.com>
2017-09-25 00:28:16 +0500

    Reformat code

--- src/Exception/ExchangeException.php
+++ src/Exception/ExchangeException.php
@@ -11,12 +11,12 @@ class ExchangeException extends MoneyException
 
     public static function createFromCurrencies(Currency $baseCurrency, Currency $counterCurrency)
     {
-        $message = sprintf(
-            'Cannot exchange a currency pair: %s/%s',
-            $baseCurrency->getCode(),
-            $counterCurrency->getCode()
+        return new self(
+            sprintf(
+                'Cannot exchange a currency pair: %s/%s',
+                $baseCurrency->getCode(),
+                $counterCurrency->getCode()
+            )
         );
-
-        return new self($message);
     }
 }
